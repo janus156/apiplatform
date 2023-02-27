21e05616ae6a90f9a626c88005508f22e60da0d2
janus156 <1569361808@qq.com>
2023-02-27 14:04:11 +0800

    添加了得到时间的接口

---
+++ src/main/java/com/platform/project/controller/TimeController.java
@@ -0,0 +1,21 @@
+package com.platform.project.controller;
+
+import clientsdk.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+
+@RestController
+@RequestMapping("/name")
+public class TimeController {
+
+    @PostMapping("/user")
+    public String getNameByPostRestful(@RequestBody User user, HttpServletRequest request){
+        return user+":"+LocalDateTime.now().toString();
+    }
+}
