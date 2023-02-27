b426b653ebcee14cfda56239f797beb579267114
janus156 <1569361808@qq.com>
2023-02-27 20:00:04 +0800

    添加了邮件发送功能

--- src/test/java/com/platform/project/service/client.java
+++ src/test/java/com/platform/project/service/client.java
@@ -3,6 +3,7 @@ package com.platform.project.service;
 import clientsdk.client.PlatClient;
 
 import clientsdk.model.User;
+import com.platform.project.controller.MailController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,10 +14,19 @@ public class client {
     @Resource
     PlatClient platClient;
 
+    @Resource
+    MailController mailController;
+
     @Test
     void getInter(){
         User user = new User();
         user.setUsername("abc");
         String usernameByPost = platClient.getUsernameByPost(user);
     }
+
+    @Test
+    void sendMail(){
+        mailController.sendTextMailMessage("3527813190@qq.com","nihao","nihao");
+    }
+
 }
